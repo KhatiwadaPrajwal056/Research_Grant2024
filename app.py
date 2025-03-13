951e5d8b838e34ed63acb82d08839b3243fac6c4
KhatiwadaPrajwal056 <khatiwadaprajwal22@icloud.com>
2025-03-13 23:19:02 +0545

    req fixed

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from components.about import show_about_section
 from dotenv import load_dotenv
 import os
 
-os.system("streamlit run app.py --server.port 8000 --server.address 0.0.0.0")
+os.system("streamlit run app.py --server.port 8000 --server.address localhost")
 
 def main():
     load_dotenv()
